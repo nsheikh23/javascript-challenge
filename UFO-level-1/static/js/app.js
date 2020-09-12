2e396251b7ba481d0afb5613432a9304e4869ba0
nsheikh23 <nabi.sheikh23@gmail.com>
2020-09-12 16:35:40 -0700

    Appended table with new rows and data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,25 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Using d3 to select the table
+var table = d3.select('table');
+
+// Using d3 to select the table body
+var tbody = d3.select('tbody');
+
+// Using d3 to append one table row `tr` for each sighting object
+tableData.forEach((sighting) => {
+    // console.log(sighting);
+    var row = tbody.append("tr");
+
+    // Using `Object.entries` to console.log each sighting's value
+    Object.entries(sighting).forEach(([key, value]) => {
+        // console.log(key, value);
+
+        // Append a cell to the row for each value
+        // in the weather report object
+        var cell = row.append("td").text(value);
+
+    });
+});
 

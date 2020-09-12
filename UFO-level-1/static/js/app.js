ecdea1a638925f42a0e76bcd57595e73d2f918d5
nsheikh23 <nabi.sheikh23@gmail.com>
2020-09-12 16:56:13 -0700

    Finished level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -23,3 +23,50 @@ tableData.forEach((sighting) => {
     });
 });
 
+// Select the button
+var button = d3.select('button');
+// console.log(button);
+
+// Select the form
+var form = d3.select('form');
+// console.log(form);
+
+// Create event handler 
+button.on("click", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    // console.log(inputValue);
+  
+    var filteredData = tableData.filter(date => date.datetime === inputValue);
+    // console.log(filteredData);
+
+    // clearing out all table rows
+    tbody.html('')
+
+    // append filtered date to the table
+    filteredData.forEach((sighting) => {
+        // console.log(sighting);
+        var row = tbody.append("tr");
+    
+        // Using `Object.entries` to console.log each sighting's value
+        Object.entries(sighting).forEach(([key, value]) => {
+            // console.log(key, value);
+    
+            // Append a cell to the row for each value
+            // in the weather report object
+            var cell = row.append("td").text(value);
+    
+        });
+    });
+}
